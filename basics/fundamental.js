3539ac715d606814efab1614e2b856dc7e0d3114
Chanchal <123716653+Chanchal82@users.noreply.github.com>
2025-03-11 07:43:17 +0000

    container config done

---
+++ basics/fundamental.js
@@ -0,0 +1,15 @@
+console.log("Chanchal")
+const id=123446;
+let email="random123@gmail.com"
+var password="654" 
+city="Ratlam"
+let accountstate;
+// if we declare anything and don't initialize it it's value is undefined
+
+console.log(id);
+
+email="rain@gmail.com";
+password="980"
+city="Bhopal"
+console.table([id,email,password,city,accountstate]);
+// prefer not to use var because of issue in block and functional scope
